5a720b7f589ab09c9ad90854c670f795c0355f8c
Gerald Hitz <gerald.hitz@gmail.com>
2026-08-07 22:07:00 +0200

    .

--- tests/Rejector.Core.Tests/obj/Debug/net10.0/Rejector.Core.Tests.AssemblyInfo.cs
+++ tests/Rejector.Core.Tests/obj/Debug/net10.0/Rejector.Core.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rejector.Core.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d022d3e05e259f52915c17547c1311bd6fab517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619a8e9125643c078c5584984140e2622848e592")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rejector.Core.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rejector.Core.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
